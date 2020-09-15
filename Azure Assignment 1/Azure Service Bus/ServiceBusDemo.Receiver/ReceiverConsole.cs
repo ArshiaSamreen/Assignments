8e9202e5f74310d9d6c38e71ff05866661918887
Arshia Samreen <arshiasamreen555@gmail.com>
2020-09-15 16:01:59 +0530

    Updated Azure Service Bus

---
+++ Azure Assignment 1/Azure Service Bus/ServiceBusDemo.Receiver/ReceiverConsole.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusDemo.Receiver
+{
+    class ReceiverConsole
+    {
+        //ToDo: Enter a valid Service Bus connection string
+        static string ConnectionString = "Endpoint=sb://servicebusnamespacedemo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WuOX453tyktn4YAc8yUXHuwfs5ayxWxUR2f55vl97iQ=";
+        static string QueuePath = "queuedemo";
+
+        
+
+        static void Main(string[] args)
+        {
+            // Create a queue client
+            var queueClient = new QueueClient(ConnectionString, QueuePath);
+
+            // Create a message handler to receive messages
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, ExceptionReceivedHandler);
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+
+            // Close the client
+            queueClient.CloseAsync().Wait();
+        }
+
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Deserialize the message body.
+            var text = Encoding.UTF8.GetString(message.Body);
+            Console.WriteLine($"Received: { text }");
+        }
+
+
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Azure Assignment 1/Azure Service Bus/ServiceBusDemo.Sender/SenderConsole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusDemo.Sender
+{
+    class SenderConsole
+    {
+        //ToDo: Enter a valid Service Bus connection string
+        static string ConnectionString = "Endpoint=sb://servicebusnamespacedemo.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=WuOX453tyktn4YAc8yUXHuwfs5ayxWxUR2f55vl97iQ=";
+        static string QueuePath = "queuedemo";
+
+
+        static void Main(string[] args)
+        {
+
+            // Create a queue client
+            var queueClient = new QueueClient(ConnectionString, QueuePath);
+
+            // Send some messages
+            for (int i = 0; i < 10; i++)
+            {
+                var content = $"Message: { i }";
+                var message = new Message(Encoding.UTF8.GetBytes(content));
+                queueClient.SendAsync(message).Wait();
+                Console.WriteLine("Sent: " + i);
+            }
+
+            // Close the client
+            queueClient.CloseAsync().Wait();
+            Console.WriteLine("Sent messages...");
+            Console.ReadLine();
+
+        }
+    }
+}
