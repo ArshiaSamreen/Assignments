6b38f5069c182c251406be26fa297ead39140697
Arshia Samreen <arshiasamreen555@gmail.com>
2020-09-16 01:06:47 +0530

    Added Azure key Vault

---
+++ Azure Assignment 2/Ecommerce.API.KeyVault/Ecommerce.Helpers/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Helpers
+{
+    public enum Category
+    {
+        Clothing, Footwear, Bags, Books, Furniture
+    }
+}
